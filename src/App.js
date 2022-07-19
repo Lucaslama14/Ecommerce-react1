471e31819c2f3b100eda669edef7affb117b3f29
lucaslama14 <Lucaslamanna14@gmail.com}>
2022-07-19 16:28:12 -0300

    Entrega nro2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import NavBar from './componentes/NavBar/NavBar';
 
 function App() {
+  const styleApp = { padding: '10px 20px', margintop : 10}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //JSX 
+    <div className="App" style={styleApp} >
+      <NavBar />
+      <h1>Flor de Ceramica</h1>
     </div>
   );
 }
---
+++ src/componentes/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return(
+        <div>
+            <h1>Logo</h1>
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Nosotros</li>
+                <li>Contacto</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
